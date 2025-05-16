152a83157c8b1d9152e848158a5710a1efe584e4
5to Bachillerato A <5to Bachillerato A@DESKTOP-MR3I3QR>
2025-05-16 10:38:21 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Factorial_SA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text = new System.Windows.Forms.Label();
+            this.txtnumero = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.text.Location = new System.Drawing.Point(12, 26);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(302, 39);
+            this.text.TabIndex = 0;
+            this.text.Text = "Ingrese un numero";
+            // 
+            // txtnumero
+            // 
+            this.txtnumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.txtnumero.Location = new System.Drawing.Point(320, 26);
+            this.txtnumero.Name = "txtnumero";
+            this.txtnumero.Size = new System.Drawing.Size(160, 45);
+            this.txtnumero.TabIndex = 1;
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button.Location = new System.Drawing.Point(63, 91);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(362, 46);
+            this.button.TabIndex = 2;
+            this.button.Text = "Calcular factorial";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(63, 143);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(353, 212);
+            this.listBox1.TabIndex = 3;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.lblresultado.Location = new System.Drawing.Point(65, 358);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(109, 39);
+            this.lblresultado.TabIndex = 4;
+            this.lblresultado.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 450);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.txtnumero);
+            this.Controls.Add(this.text);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label text;
+        private System.Windows.Forms.TextBox txtnumero;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblresultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial_SA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            lblresultado.Text = "";
+            if (int.TryParse(txtnumero.Text, out int num))
+            {
+                long factorial = 1;
+                string multiplicacion = "";
+                for (int i = num; i > 0; i--)
+                {
+                    factorial = factorial * i;
+                    listBox1.Items.Add(i);
+
+                    multiplicacion += (i == 1) ? $"{i}" : $"{i}";
+                }
+                lblresultado.Text = $"{num}! = {multiplicacion} = {factorial}";
+            }
+           else
+            {
+                MessageBox.Show("ingrese un numero valido");
+            }
+        }
+    }
+}
